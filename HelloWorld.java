794af6d606d79adb2806bca4d3b12203f0cca85a
PavaniKempula <pavanikempula1998@gmail.com>
2021-05-31 08:47:31 +0530

    Added : Feature of adding two numbers

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,4 +2,7 @@ class HelloWorld {
           public static void main(String args[]) {
              System.out.println("Hello World");
           }
+          public int add(int a, int b) {
+             return a+b;
+          }
 }
