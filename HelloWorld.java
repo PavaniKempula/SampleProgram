e2ebea24e7dc09d4ccbd042533e730bff3082b9b
PavaniKempula <pavanikempula1998@gmail.com>
2021-06-01 18:17:20 +0530

    Added :Subtraction operation is added

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,4 +5,8 @@ class HelloWorld {
           public int add(int a, int b) {
              return a+b;
           }
+           public int sub(int a, int b) {
+             return b-a;
+          }
+
 }
